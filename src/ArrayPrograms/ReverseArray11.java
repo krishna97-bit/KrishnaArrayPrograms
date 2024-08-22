25ed7aec6880d10d9791964e7b7f019cd001ede6
indralekrishna1997@gmail.com <indralekrishna1997@gmail.com>
2024-08-22 11:49:01 +0530

    add array programs

---
+++ src/ArrayPrograms/ReverseArray11.java
@@ -0,0 +1,29 @@
+package ArrayPrograms;
+
+import java.util.Arrays;
+
+public class ReverseArray11 {
+
+    public static void main(String[] args) {
+
+        int[] a = {10, 20, 30, 40, 50};
+        int[] b = new int[5];
+        for (int i = a.length - 1; i >= 0; i--) {
+
+            b[i] = a[i];
+            }
+        System.out.println(Arrays.toString(b));
+        System.out.println("Print this statement");
+        System.out.println("Print array programs");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
